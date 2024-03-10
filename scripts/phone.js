3c6c18a0bd4d1d54d5c70b6f140801ef1b779861
NewazRafi <mdnsrafi@gmail.com>
2024-03-10 08:56:52 +0600

    search functionality added

--- scripts/phone.js
+++ scripts/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json();
     const phones = data.data;
     // console.log(phones);
@@ -10,12 +10,14 @@ const displayPhones = phones => {
     // console.log(phones);
     // 1)
     const phoneContainer = document.getElementById('phone-container');
+    // clear phone container cards
+    phoneContainer.textContent = '';
 
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         // 2) create a div
         const phoneCard = document.createElement('div');
-        phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+        phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`;
         // 3) set inner HTML
         phoneCard.innerHTML = `
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
@@ -30,6 +32,14 @@ const displayPhones = phones => {
         // 4) append child
         phoneContainer.appendChild(phoneCard)
     })
+}
+
+// handle search button
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
 }
 
 loadPhone();
