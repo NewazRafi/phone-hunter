d456f09cfe8b316464a01c3cbe09587a75077f56
NewazRafi <mdnsrafi@gmail.com>
2024-03-12 06:53:04 +0600

    show all button functionality and modal added

--- scripts/phone.js
+++ scripts/phone.js
@@ -39,8 +39,8 @@ const displayPhones = (phones, isShowAll) => {
         <div class="card-body">
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+            <div class="card-actions justify-center">
+                <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
             </div>
         </div>
         `;
@@ -50,6 +50,25 @@ const displayPhones = (phones, isShowAll) => {
 
     // hide loading infinity
     toggleLoader(false);
+}
+
+// handle show detail button
+const handleShowDetails = async (id) =>{
+    // console.log('clicked', id);
+    // load single phone data
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+
+    showPhoneDetails(phone);
+}
+
+const showPhoneDetails = (phone) =>{
+    console.log(phone);
+    const phoneName = document.getElementById('show-detail-phone-name');
+    phoneName.innerText = phone.name;
+    // show the modal
+    show_details_modal.showModal();
 }
 
 // handle search button
